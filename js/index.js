30d9bf592ed623f19c6f8c1c35f12fd2adc68457
kumar <rjbaltarlopezcozar@gmail.com>
2019-03-24 11:21:56 +0100

    commint uno

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * La función $(document).ready() de jQuery 
+ * ejecuta este código cuando se carga totalmente
+ * la página (index.html).
+ */
+
+$(document).ready(function() {
+
+    $.controlador.init("panel_main");
+});
